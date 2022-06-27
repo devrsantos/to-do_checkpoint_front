cc58cfc1f2f66a235f1dc7b1969ec2aca7d7099b
Renan Augusto dos Santos <devrsantos@gmail.com>
2022-06-27 11:15:17 -0300

    Ajustes Apenas

--- scripts/scriptLogin.js
+++ scripts/scriptLogin.js
@@ -1,5 +1,3 @@
-const inputEmail = document.querySelector("#inputEmail");
-const inputSenha = document.querySelector("#inputSenha");
 const btnEnviar = document.querySelector("#btnEnviar");
 const inputEmail = document.querySelector("#inputEmail");
 const inputSenha = document.querySelector("#inputPassword");
@@ -21,10 +19,79 @@ const icon = document.querySelector("#eye");
     }
  });
 */
+
+// _______________ email________________
+const validaEmail = str => {
+    let email = str.value;
+    
+    const re = /\S+@\S+\.\S+/;
+    
+    if (!re.test(email)) {
+        str.classList.remove('success');
+        str.classList.add('error');
+        return false;
+    } else {
+        str.classList.remove('error');
+        str.classList.add('success');
+        str.style.border.color = "green"
+        return true;
+    }
+    
+};
+
+
+// _______________ senha ________________
+const validaSenha = str => {
+    let senha = str.value;
+    if(senha.length==0 || senha === '' || senha.length<=6){
+        str.classList.remove('success');
+        str.classList.add('error');
+        return false;  
+    } else {
+        str.classList.remove('error');
+        str.classList.add('success');
+    
+        return true; 
+    }
+    
+};
+
+
+// _______________ confirma senha ________________
+const confirmaSenha = (pass1, pass2) => {
+    let senha1 = pass1.value;    
+    let senha2 = pass2.value;
+
+    if(senha1!=senha2 || senha2==='' ){
+        pass2.classList.remove('success');
+        pass2.classList.add('error');
+        return false;  
+    }else if(senha1==senha2){
+        pass2.classList.remove('error');
+        pass2.classList.add('success');
+        return true; 
+    }
+    
+};
+
+const validaCampos = () => {
+    validaEmail(inputEmail);
+    validaSenha(inputSenha);
+    confirmaSenha(inputSenha, inputConfirmaSenha);
+};
+
+
 // ________________________________ post usuário/login_________________________________
 function fetchAPI(){
+
+    console.log(JSON.stringify({
+        email: `${inputEmail.value}`,
+        password: `${inputSenha.value}`,
+     }));
+
+
     fetch('https://ctd-todo-api.herokuapp.com/v1/users',{
-        method: 'GET',
+        method: 'POST',
         headers:{
             'Accept': '*/* , application/json, text/plain',
             'Content-Type': 'application/json'
@@ -43,9 +110,7 @@ function fetchAPI(){
 btnEnviar.addEventListener("click", e => {
     e.preventDefault();
 
-   /* if (
-        !validaString(inputNome) &&
-        !validaString(inputSobrenome) &&
+    if (
         !validaEmail(inputEmail) &&
         !validaSenha(inputSenha) &&
         !confirmaSenha(inputSenha, inputConfirmaSenha)
@@ -61,5 +126,4 @@ btnEnviar.addEventListener("click", e => {
         
         // Aqui será adicionado o redicionamento para a página de tarefa
     }
-    */
 });
